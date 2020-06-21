fe1143f31e00de06c953e9430452f4dd8debdbff
pang <kkks94@naver.com>
2020-06-21 21:20:36 +0900

    first commit

---
+++ 구구단.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class 구구단 {
+
+	public static void main(String[] args) {
+
+		int a;
+		
+		
+		Scanner in = new Scanner(System.in);
+		
+		a = in.nextInt();
+		
+		for(int b = 1; b < 11; b++) {
+			
+			System.out.println(a+" x "+b+" = "+a * b);
+			
+			
+			
+			
+		}
+		
+	}
+
+}
